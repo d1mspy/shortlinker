9dce86ba221dd442515b73454f62dece91a93a00
d1mspy <rom.dog20@gmail.com>
2024-10-30 19:04:02 +0300

    add description to swagger + exeption 404

--- shortlinker/presentations/app.py
+++ shortlinker/presentations/app.py
@@ -1,31 +1,47 @@
-from fastapi import FastAPI, Response, status, Path
+from fastapi import FastAPI, Response, status, Path, HTTPException
 from pydantic import BaseModel
 from services.short_link_service import ShortLinkService
 
-app = FastAPI()
+app = FastAPI(
+    title='Сервис генерации коротких ссылок',
+    description='простенький тестовый сервис для создания коротких ссылок'
+    )
 short_link_service = ShortLinkService()
 
 
 @app.get("/health")
 def hello_world() -> str:
+    """
+    тестовый эндпоинт
+    """
     return "ok"
 
 
 class PutLink(BaseModel):
+    """
+    ссылка 
+    """
     link: str
 
 
 @app.put("/link")
 def put_link(long_link: PutLink) -> PutLink:
+    """
+    метод создания короткой ссылки по длинной ссылке
+    """
     short_link = short_link_service.put_link(long_link.link)
-
+    
     return PutLink(link=f'http://localhost:8000/short/{short_link}')
 
 
 @app.get("/short/{short_link}")
 def get_link(short_link: str = Path(...)) -> Response:
+    """
+    метод переадресации с короткой ссылки на длинную
+    """
     long_link = short_link_service.get_link(short_link)
-
+    if long_link is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Упс, мы не нашли эту ссылочку")
     return Response(
         content=None, 
         headers={"Location": long_link}, 

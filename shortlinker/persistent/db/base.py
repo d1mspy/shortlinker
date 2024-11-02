23541e0b9cc46f904065ce9925efdd8e860be290
d1mspy <rom.dog20@gmail.com>
2024-11-02 19:18:04 +0300

    add db files

---
+++ shortlinker/persistent/db/base.py
@@ -0,0 +1,13 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+Base.metadata = MetaData(schema="public")
+
+class link(Base):
+    ...
+
+class LinkUsage(Base):
+    ...
+
+    

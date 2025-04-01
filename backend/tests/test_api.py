6674b596fb85e8d5665cb6800bcf950c46017cef
d1mspy <subbotin2126@gmail.com>
2025-04-01 11:51:39 +0300

    feat: add test

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,12 +1,8 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
-from presentations.app import app
 
-@pytest.mark.asyncio
-async def test_get_all_links():
-    async with AsyncClient(
-        transport=ASGITransport(app=app),
-        base_url="http://test"
-        ) as ac:
-        response = await ac.get("/link/list")
-        print(response)
+def func():
+    return "test is working"
+
+def test_func():
+    assert func() == "test is working"
